f63e93a161b16c45aab526d26df408072d5f41e7
rajarajasozhanh <35727808+rajarajasozhanh@users.noreply.github.com>
2018-02-08 16:37:48 +0530

    Create leep.c

---
+++ leep.c
@@ -0,0 +1,18 @@
+
+#include <stdiod.h>
+void main()
+{
+  int l;
+  printf(" entr the year");
+  scanf(" %d",&l);
+  if(l%4==0)
+  {
+   printf("  its leap year \n");
+   }
+   else
+   {
+   printf(" its not leap year");
+   }
+   getch();
+}
+ 
